c9cd328c2496b3eee9052dd20dac1889c8cd5f22
ruglcc <ruglcc@qq.com>
2019-06-13 13:02:27 +0800

    updata

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(
     proxy("/api", {
-      target: "https://www.yiqibangbang.com:8080/",
+      target: "https://www.yiqibangbang.com:8080",
       changeOrigin: true, // needed for virtual hosted sites
       ws: true, // proxy websockets
       pathRewrite: {
